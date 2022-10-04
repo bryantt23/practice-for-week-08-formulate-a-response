c04ffd4045782def9090bdf7a095a5495a02063d
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-04 00:34:57 -0700

    return html

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 // Your code here
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const responseBody = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h1>Hello there!</h1>
+  </body>
+  </html>    
+`;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(responseBody);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
